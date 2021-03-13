7eb5f0c6d8d4f49879254dae34d6a4de3fda797d
nofishtou <damnedbyhimself@gmail.com>
2021-03-13 11:09:16 +0200

    feat: add task directives

---
+++ src/app/core/directives/app-high-light-bg.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AppHighLightBgDirective } from './app-high-light-bg.directive';
+
+describe('AppHighLightBgDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AppHighLightBgDirective();
+    expect(directive).toBeTruthy();
+  });
+});
